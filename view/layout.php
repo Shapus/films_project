5511014057413039f8b873ed818af8895efc0e9f
pupil <pupil@ivkhk.ee>
2020-10-01 13:41:10 +0300

    bootstrap styles 1

--- view/layout.php
+++ view/layout.php
@@ -5,108 +5,112 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="scss/dist/styles.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="js/script.js"></script>
     <title>Document</title>
 </head>
 <body>
-    <div class="wrapper column">
-        <header class="header">
-            <div class="container container--header">
-                <div class="header__inner">                    
-                    <div class="header__top-line row row--center">
-                        <div class="header__menu">
-                            <div class="menu">
-                                <ul class="menu__list row row--center">
-                                    <div class="menu__dropbox">
-                                        <div class="menu__dropbtn-box">
-                                            <a class="menu__dropbtn" href="#">Фильмы</a>
-                                        </div>
-                                        <ul class="menu__drop-content">
-                                            <li class="menu__item row row--center">
-                                                <a href="" class="menu__link row row--center">film_genre_1</a>
-                                            </li>
-                                            <li class="menu__item row row--center">
-                                                <a href="" class="menu__link row row--center">film_genre_2</a>
-                                            </li>
-                                            <li class="menu__item row row--center">
-                                                <a href="" class="menu__link row row--center">film_genre_3</a>
-                                            </li>
-                                        </ul>
-                                    </div> 
-                                    <div class="menu__dropbox">
-                                        <div class="menu__dropbtn-box">
-                                            <a class="menu__dropbtn" href="#">Сериалы</a>
-                                        </div>
-                                        <ul class="menu__drop-content">
-                                            <li class="menu__item row row--center">
-                                                <a href="" class="menu__link row row--center">series_genre_1</a>
-                                            </li>
-                                            <li class="menu__item row row--center">
-                                                <a href="" class="menu__link row row--center">series_genre_2</a>
-                                            </li>
-                                            <li class="menu__item row row--center">
-                                                <a href="" class="menu__link row row--center">series_genre_3</a>
-                                            </li>
-                                        </ul>
-                                    </div> 
-                                    <div class="menu__dropbox">
-                                        <div class="menu__dropbtn-box">
-                                            <a class="menu__dropbtn" href="#">О нас</a>
-                                        </div>
-                                    </div> 
-                                </ul>
+    <div class="wrapper container-fluid d-flex justify-content-center">
+        <div class="wrapper-inner container d-flex flex-column">
+            <header class="header d-flex">
+                <div class="container container--header">
+                    <div class="header__inner">                    
+                        <div class="header__top-line row row--center">
+                            <div class="header__menu">
+
+                                <div class="menu">
+                                    <ul class="menu__list row row--center">
+                                        <div class="menu__dropbox">
+                                            <div class="menu__dropbtn-box">
+                                                <a class="menu__dropbtn" href="#">Фильмы</a>
+                                            </div>
+                                            <ul class="menu__drop-content">
+                                                <li class="menu__item row row--center">
+                                                    <a href="" class="menu__link row row--center">film_genre_1</a>
+                                                </li>
+                                                <li class="menu__item row row--center">
+                                                    <a href="" class="menu__link row row--center">film_genre_2</a>
+                                                </li>
+                                                <li class="menu__item row row--center">
+                                                    <a href="" class="menu__link row row--center">film_genre_3</a>
+                                                </li>
+                                            </ul>
+                                        </div> 
+                                        <div class="menu__dropbox">
+                                            <div class="menu__dropbtn-box">
+                                                <a class="menu__dropbtn" href="#">Сериалы</a>
+                                            </div>
+                                            <ul class="menu__drop-content">
+                                                <li class="menu__item row row--center">
+                                                    <a href="" class="menu__link row row--center">series_genre_1</a>
+                                                </li>
+                                                <li class="menu__item row row--center">
+                                                    <a href="" class="menu__link row row--center">series_genre_2</a>
+                                                </li>
+                                                <li class="menu__item row row--center">
+                                                    <a href="" class="menu__link row row--center">series_genre_3</a>
+                                                </li>
+                                            </ul>
+                                        </div> 
+                                        <div class="menu__dropbox">
+                                            <div class="menu__dropbtn-box">
+                                                <a class="menu__dropbtn" href="#">О нас</a>
+                                            </div>
+                                        </div> 
+                                    </ul>
+                                
+                                </div>
+                            
                             </div>
-                        </div>
-                        <div class="header__search row row--center">
-                            <div class="search_box search_box--header">
-                                <input class="search_box__input search_box__input--header" type="text">
+                            <div class="header__search row row--center">
+                                <div class="search_box search_box--header">
+                                    <input class="search_box__input search_box__input--header" type="text">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </header>
-        <main class="main">
-            <div class="container container--main">
-                <div class="main__inner row">  
-                <section class="favorites row">
-                        <div class="container container--section-container container--favorites">
-                            <div class="favorites__inner">
-                                <div class="favorites__item column column--center">
-                                    <img class="favorites__img" src="images/no_img.jpg" alt="favorites_image_1">
-                                    <div class="favorites__title">favorites_title_1akdagdkjaghdkjagksfhsgfkj,hzsgfkj,sgdfk,jhsgdkdg</div>
-                                </div>
-                                <div class="favorites__item column column--center">
-                                    <img class="favorites__img" src="images/no_img.jpg" alt="favorites_image_2">
-                                    <div class="favorites__title">favorites_title_2</div>
-                                </div>
-                                <div class="favorites__item column column--center">
-                                    <img class="favorites__img" src="images/no_img.jpg" alt="favorites_image_3">
-                                    <div class="favorites__title">favorites_title_3</div>
-                                </div>
-                                <div class="favorites__item column column--center">
-                                    <img class="favorites__img" src="images/no_img.jpg" alt="favorites_image_4">
-                                    <div class="favorites__title">favorites_title_4</div>
-                                </div>
-                                <div class="favorites__item column column--center">
-                                    <img class="favorites__img" src="images/no_img.jpg" alt="favorites_image_5">
-                                    <div class="favorites__title">favorites_title_5</div>
+            </header>
+            <main class="main d-flex">
+                <div class="main__inner d-flex justify-content-center">  
+                    <section class="favorites d-flex">
+                            <div class="container container--section-container container--favorites">
+                                <div class="favorites__inner">
+                                    <div class="favorites__item column column--center">
+                                        <img class="favorites__img" src="images/no_img.jpg" alt="favorites_image_1">
+                                        <div class="favorites__title">favorites_title_1akdagdkjaghdkjagksfhsgfkj,hzsgfkj,sgdfk,jhsgdkdg</div>
+                                    </div>
+                                    <div class="favorites__item column column--center">
+                                        <img class="favorites__img" src="images/no_img.jpg" alt="favorites_image_2">
+                                        <div class="favorites__title">favorites_title_2</div>
+                                    </div>
+                                    <div class="favorites__item column column--center">
+                                        <img class="favorites__img" src="images/no_img.jpg" alt="favorites_image_3">
+                                        <div class="favorites__title">favorites_title_3</div>
+                                    </div>
+                                    <div class="favorites__item column column--center">
+                                        <img class="favorites__img" src="images/no_img.jpg" alt="favorites_image_4">
+                                        <div class="favorites__title">favorites_title_4</div>
+                                    </div>
+                                    <div class="favorites__item column column--center">
+                                        <img class="favorites__img" src="images/no_img.jpg" alt="favorites_image_5">
+                                        <div class="favorites__title">favorites_title_5</div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                     </section>  
-                                                
-                    <section class="content">
+                                                    
+                    <section class="content d-flex">
                         <div class="container container--section-container container--content">
                             <div class="content__inner">
                             <?php 
-				                if(isset($content)) echo $content;
+                                if(isset($content)) echo $content;
                                 else echo "<h1>Ups, no content!</h1>";
-			                ?>
+                            ?>
                             </div>
                         </div>
                     </section>
-                    <section class="left-menu">
+                    <section class="left-menu d-flex">
                         <div class="container container--section-container container--left-menu">
                             <div class="left-menu__inner">
                                 <div class="logo logo--left-menu row row--center">ЛОГОТИП</div>
@@ -126,13 +130,8 @@
                         </div>
                     </section>     
                 </div> <!--main inner-->
-            </div> <!--main container-->
-        </main>
-        <footer class="footer">
-            <div class="container container--footer">
-                <div class="footer__inner"></div>
-            </div>
-        </footer>
+            </main>
+        </div> <!--wrapper-inner-->
     </div>  <!--wrapper-->
 </body>
 </html>
